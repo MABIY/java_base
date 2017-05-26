969e63947c54b33f026eaaf7e58d0866a25f9427
skylark <skylark.liu@movit-tech.com>
2017-05-26 14:47:23 +0800

    ..

---
+++ java编程思想/src/第二章/Practice_1.java
@@ -0,0 +1,13 @@
+package 第二章;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * Created by lh on 17-5-26.
+ */
+public class Practice_1 {
+    public static void mian(String[] args) {
+        System.out.println("Hello World?");
+         print("Hello World?");
+    }
+}
