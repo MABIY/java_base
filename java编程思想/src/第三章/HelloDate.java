16467296f86b6ffff1e7784cbf762ceb8f9daa78
lh <jyfc7879@gmail.com>
2017-06-14 22:31:39 +0800

    ..

--- java编程思想/src/第三章/HelloDate.java
+++ java编程思想/src/第三章/HelloDate.java
@@ -1,15 +1,19 @@
 package 第三章;
 
 import java.util.Date;
+import java.util.Locale;
 
 import static net.mindview.util.Print.print;
 
 /**
- * Created by lh on 17-5-26.
+ * Created by lh on 17-6-14.
  */
 public class HelloDate {
     public static void main(String[] args) {
-        print("Hello , it's");
+        print("Hello, it's");
         print(new Date());
+        int i = 461012;
+        System.out.printf("The value of i is : %d%n" , i);
+        System.out.println("HelloDate.main");
     }
 }
--- java编程思想/src/第三章/Practice_1.java
+++ java编程思想/src/第三章/Practice_1.java
@@ -1,13 +1,14 @@
 package 第三章;
 
 import static net.mindview.util.Print.print;
+import static sun.misc.Version.println;
 
 /**
- * Created by lh on 17-5-26.
+ * Created by lh on 17-6-14.
  */
 public class Practice_1 {
     public static void main(String[] args) {
-        System.out.println("Hello World?");
-         print("Hello World?");
+        print("Hello , from short form.");
+        System.out.println("Hello from long form.");
     }
 }
