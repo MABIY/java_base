987c93cee3625e3fa59d4c30b2e5381c4f28244a
lh <jyfc7879@gmail.com>
2017-06-23 14:08:55 +0800

    ..

---
+++ java编程思想/src/第六章/练习题/Collision.java
@@ -0,0 +1,13 @@
+package 第六章.练习题;
+
+import net.mindview.simple.*;
+import java.util.*;
+
+/**
+ * Created by lh on 17-6-23.
+ */
+public class Collision {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+    }
+}
