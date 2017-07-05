de915a554cfadb2bc667c977ad79123788f160bb
lh <jyfc7879@gmail.com>
2017-07-05 09:48:20 +0800

    add

---
+++ java编程思想/src/第十章/Sequence.java
@@ -0,0 +1,73 @@
+package 第十章;
+
+/**
+ * Created by lh on 17-7-5.
+ */
+interface Selector {
+    boolean end();
+
+    Object current();
+
+    void next();
+}
+public class Sequence {
+    private Object[] items;
+    private int next = 1;
+
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+
+    public void add(Object x) {
+        if (next < items.length) {
+            items[next++] = x;
+        }
+    }
+
+    public class SequenceSelector implements Selector {
+
+        public SequenceSelector() {
+        }
+
+        private int i= 0;
+        @Override
+        public boolean end() {
+            return i== items.length;
+        }
+
+        @Override
+        public Object current() {
+            return items[i];
+        }
+
+        @Override
+        public void next() {
+            if (i < items.length) {
+                i++;
+            }
+        }
+
+        public int getNext() {
+            return next;
+        }
+    }
+
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(10);
+        for (int i = 0; i < 10; i++) {
+            sequence.add(Integer.toString(i));
+        }
+        Selector selector = sequence.selector();
+        while (!selector.end()) {
+            System.out.println(selector.current()+" ");
+            selector.next();
+        }
+    }
+
+}
+
+
